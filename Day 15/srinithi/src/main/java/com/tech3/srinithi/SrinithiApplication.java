78b4fce8631aefeb5672d8bb5bdb196d54747484
Reegan1 <104960836+Reegan1@users.noreply.github.com>
2023-10-12 15:19:26 +0530

    Add files via upload

---
+++ Day 15/srinithi/src/main/java/com/tech3/srinithi/SrinithiApplication.java
@@ -0,0 +1,13 @@
+package com.tech3.srinithi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrinithiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrinithiApplication.class, args);
+	}
+
+}
